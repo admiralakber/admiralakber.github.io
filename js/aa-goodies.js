f765f924c61ddff3d8c14f173c3ab59c4fc23f03
Aqeel Akber <aqeel.akber@gmail.com>
2018-09-25 10:13:22 +1000

    New post: Dark Theme

--- js/aa-goodies.js
+++ js/aa-goodies.js
@@ -50,12 +50,12 @@ function vis_toggle(id) {
 function dark_toggle() {
     var el1 = document.getElementById("dark-reader");
     var icon = document.getElementById("dark-reader-icon");
-    if( el1.getAttribute("disabled") == "disabled") {
-        el1.removeAttribute("disabled");
+    if(el1.disabled) {
+        el1.disabled = false;
         icon.setAttribute("class", "fas fa-sun fa-pulse");
         localStorage.setItem("darkreader", "enabled");
     } else {
-        el1.setAttribute("disabled", "disabled");
+        el1.disabled = true;
         icon.setAttribute("class", "fas fa-moon fa-pulse");
         localStorage.setItem("darkreader", "disabled");
     }
